2b4a6d9c38e37aa2e55535d016fbd6bddfb6d973
HassanHI3  <Hassanhi3@icloud.com>
2026-09-24 22:48:14 +0100

    fixed aws region format in backend.tf

--- ec2-legacy-app/terraform-ecs/backend.tf
+++ ec2-legacy-app/terraform-ecs/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "hassan-terraform-state-bucket"
     key          = "ec2-to-ecs-migration/production/terraform.tfstate"
-    region       = eu-west-2
+    region       = "eu-west-2"
     use_lockfile = true
   }
 }
